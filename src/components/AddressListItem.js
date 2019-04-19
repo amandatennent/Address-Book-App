ea924d83afdb3e7756dbfe62c8ca6ed132aec78e
Amanda Tennent <amandatennent@me.com>
2019-04-19 23:35:09 +1000

    Added necessary files to get ESLint autoformat working correctly

--- src/components/AddressListItem.js
+++ src/components/AddressListItem.js
@@ -1,25 +1,23 @@
 /* TODO: Update anchors to buttons */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import mapPointer from '../images/mappointer.svg';
 import React from 'react';
 import PropTypes from 'prop-types';
+import mapPointer from '../images/mappointer.svg';
 
-class AddressListItem extends React.Component {
-    static propTypes = {
-        name: PropTypes.string
-    }
-
-    render() {
-        return(
-            <li className="nav-item">
-                <a className="nav-link" href="#">
-                    <img src={mapPointer} alt="Map Pointer" />
-                    {this.props.name}
-                </a>
+const AddressListItem = props => {
+    return (
+        <li className="nav-item">
+            <a className="nav-link" href="#">
+                <img src={mapPointer} alt="Map Pointer" />
+                <span className="pl-2">{props.name}</span>
+            </a>
         </li>
-        );
-    }
-}
+    );
+};
+
+AddressListItem.propTypes = {
+    name: PropTypes.string,
+};
 
 export default AddressListItem;
